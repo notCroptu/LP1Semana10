ce96ef1676acac10dddc9d9afa1d27c43780bb69
notCroptu <martinsmariana1972@gmail.com>
2024-05-17 13:32:12 +0100

    Rename PlayerManagerMVC

--- PlayerManagerMVC/IView.cs
+++ PlayerManagerMVC/IView.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace PlayerManager4
+namespace PlayerManagerMVC
 {
     public interface IView
     {
--- PlayerManagerMVC/Program.cs
+++ PlayerManagerMVC/Program.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace PlayerManager4
+namespace PlayerManagerMVC
 {
     public class Program
     {
